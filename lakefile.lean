6f1d0f0a0b8926492a7dcb8d06ea72f0a3b19203
F. G. Dorais <fgdorais@gmail.com>
2023-01-05 11:40:32 -0500

    update Lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,9 +5,9 @@ package GMLAlgebra {
   moreLeanArgs := #["-Dlinter.unusedVariables=false"]
 }
 
-@[defaultTarget]
+@[default_target]
 lean_lib GMLAlgebra {}
 
-require std from git "https://github.com/leanprover/std4.git"@"e564a554a3aa40ca8542928494ef161a1f79f2df"
+require std from git "https://github.com/leanprover/std4.git"@"main"
 
-require GMLInit from git "https://github.com/fgdorais/GMLInit.git"@"c4f9ed1507ec528e64be72bd1564b93ed973a713"
+require GMLInit from git "https://github.com/fgdorais/GMLInit.git"@"main"
