0702055156f6b4a4025d59392df4692476cebd02
F. G. Dorais <fgdorais@gmail.com>
2023-02-28 23:27:30 -0500

    update Lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,4 @@ package GMLAlgebra {
 @[default_target]
 lean_lib GMLAlgebra {}
 
-require std from git "https://github.com/leanprover/std4.git"@"main"
-
-require GMLInit from git "https://github.com/fgdorais/GMLInit.git"@"main"
+require GMLInit from git "https://github.com/fgdorais/GMLInit"@"main"
